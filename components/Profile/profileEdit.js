b9f13d1600542d995b963e7b02c8e663d2b692cd
Vassardog77 <benmoxon256@gmail.com>
2023-08-23 17:02:17 -0700

    profile

--- components/Profile/profileEdit.js
+++ components/Profile/profileEdit.js
@@ -1,65 +1,102 @@
 import React, { useState } from 'react';
-import { postProfile } from '../../actions/profileActions'
+import { View, TextInput, Button, Text, StyleSheet } from 'react-native';
 import { useDispatch } from 'react-redux';
-import imageCompression from 'browser-image-compression';
+import { postProfile } from '../../actions/profileActions';
+import ImagePicker from 'react-native-image-picker'; // Corrected import statement
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 function ProfileEdit(props) {
+    const [Img1, setImg1] = useState('');
+    const [Img2, setImg2] = useState('');
+    const [Img3, setImg3] = useState('');
+    const [Img4, setImg4] = useState('');
+    const [orgName, setOrgName] = useState('');
+    const [contactInfo, setContactInfo] = useState('');
+    const [description, setDescription] = useState('');
 
-    const [Img1, setImg1] = useState('')
-    const [Img2, setImg2] = useState('')
-    const [Img3, setImg3] = useState('')
-    const [Img4, setImg4] = useState('')
-    
-    const dispatch = useDispatch()
-    const current_user = JSON.parse(localStorage.getItem('user'))
+    const dispatch = useDispatch();
 
-    let post_profile = async (e) => {
-        e.preventDefault()
-        let profile = ({
-            "user":current_user.email,
-            "img1":Img1,
-            "org_name":e.target[1].value,
-            "img2":Img2,
-            "contact":e.target[3].value,
-            "img3":Img3,
-            "description":e.target[5].value,
-            "img4":Img4
-        })
-        alert("Profile Updated!")
-        dispatch(postProfile(profile))
-    }
+    const handleImageUpload = (setImage) => {
+        ImagePicker.launchImageLibrary({ mediaType: 'photo' }, (response) => {
+            if (response.didCancel) {
+                console.log('User cancelled image picker');
+            } else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+            } else {
+                setImage(response.uri);
+            }
+        });
+    };
 
-    const handleImageUpload = async (e, setImage) => {
-        const imageFile = e.target.files[0];
-        const options = {
-          maxSizeMB: 0.25, // (0.25 MB = 250 KB), max file size
-          maxWidthOrHeight: 1920, // compress the image to 1080p
-          useWebWorker: true
-        }
-        try {
-          const compressedFile = await imageCompression(imageFile, options);
-          const base64Img = await imageCompression.getDataUrlFromFile(compressedFile);
-          setImage(base64Img)
-        } catch (error) {
-          console.log('Error occurred while compressing the image: ', error);
-        }
-      }
+    const post_profile = async () => {
+        const current_user = JSON.parse(await AsyncStorage.getItem('user'));
+        let profile = {
+            user: current_user.email,
+            img1: Img1,
+            org_name: orgName,
+            img2: Img2,
+            contact: contactInfo,
+            img3: Img3,
+            description: description,
+            img4: Img4
+        };
+
+        alert("Profile Updated!");
+        dispatch(postProfile(profile));
+    };
 
     return (
-        <div className='profile_edit'>
-            <form onSubmit={post_profile}>
-                <div>Add image(s):<input type='file' accept="image/*" onChange={(e) => handleImageUpload(e, setImg1)}/></div>
-                <div><textarea placeholder='Organization Name'></textarea></div>
-                <div>Add image(s):<input type='file' accept="image/*" onChange={(e) => handleImageUpload(e, setImg2)}/></div>
-                <div>Where to Contact Us</div>
-                <div><textarea placeholder='Contact information'></textarea></div>
-                <div>Add image(s):<input type='file' accept="image/*" onChange={(e) => handleImageUpload(e, setImg3)}/></div>
-                <div><textarea placeholder='Description'></textarea></div>
-                <div>Add image(s):<input type='file' accept="image/*" onChange={(e) => handleImageUpload(e, setImg4)}/></div>
-                <div><button type='submit'>Save Profile</button></div>
-            </form>
-        </div>
+        <View style={styles.container}>
+            <Text>Add image(s):</Text>
+            <Button title="Upload Image 1" onPress={() => handleImageUpload(setImg1)} />
+
+            <TextInput 
+                style={styles.inputField}
+                placeholder='Organization Name' 
+                value={orgName}
+                onChangeText={setOrgName}
+            />
+
+            <Text>Add image(s):</Text>
+            <Button title="Upload Image 2" onPress={() => handleImageUpload(setImg2)} />
+
+            <Text>Where to Contact Us:</Text>
+            <TextInput 
+                style={styles.inputField}
+                placeholder='Contact information' 
+                value={contactInfo}
+                onChangeText={setContactInfo}
+            />
+
+            <Text>Add image(s):</Text>
+            <Button title="Upload Image 3" onPress={() => handleImageUpload(setImg3)} />
+
+            <TextInput 
+                style={styles.inputField}
+                placeholder='Description'
+                value={description}
+                onChangeText={setDescription}
+            />
+
+            <Text>Add image(s):</Text>
+            <Button title="Upload Image 4" onPress={() => handleImageUpload(setImg4)} />
+
+            <Button title='Save Profile' onPress={post_profile} />
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 16
+    },
+    inputField: {
+        marginVertical: 10,
+        padding: 8,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 4,
+    }
+});
 
 export default ProfileEdit;
--- components/Profile/profileView.js
+++ components/Profile/profileView.js
@@ -1,13 +1,115 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, Image, StyleSheet } from 'react-native';
+import axios from 'axios';
+import { base_url } from '../../api';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
-const ProfileView = () => {
-  return (
-    <View>
-      <Text>ProfileView</Text>
-    </View>
-  );
-};
+function ProfileView(props) {
+    const navigation = useNavigation();
+    const route = useRoute();
+    const { id } = route.params || {};
 
+    const current_user = JSON.parse(localStorage.getItem('user')); // You might want to use AsyncStorage in React Native for this
+
+    const [Img1, setImg1] = useState('');
+    const [Orgname, setOrgname] = useState('');
+    const [Img2, setImg2] = useState('');
+    const [Contact, setContact] = useState('');
+    const [Img3, setImg3] = useState('');
+    const [Description, setDescription] = useState('');
+    const [Img4, setImg4] = useState('');
+    const [noProfile, setNoProfile] = useState(false);
+
+    const fetchData = async () => { 
+        const email = id ? id : current_user.email;
+
+        await axios.post(base_url+'/profiles/get', { data: email })
+        .then((response) => {
+            if (response.data) {
+                setImg1(response.data.img1);
+                setOrgname(response.data.org_name);
+                setImg2(response.data.img2);
+                setContact(response.data.contact);
+                setImg3(response.data.img3);
+                setDescription(response.data.description);
+                setImg4(response.data.img4);
+                setNoProfile(false);
+            } else {
+                setNoProfile(true);
+            }
+        });
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const messageLink = () => {
+        const room = [current_user.email, id].sort().join(', ');
+        navigation.navigate('Messages', { room });
+    };
+
+    return (
+        <View style={styles.profile}>
+            {id && (
+                <Button title="Message" onPress={messageLink} />
+            )}
+            {noProfile ? (
+                <Text>No profile yet</Text>
+            ) : (
+                <>
+                    <Image source={{ uri: Img1 }} style={styles.profileLogo} />
+                    <Text style={styles.profileOrgname}>{Orgname}</Text>
+                    <View style={styles.profileMaincontent}>
+                        <Text style={styles.profileContact}>{Contact}</Text>
+                        <Image source={{ uri: Img2 }} style={styles.image} />
+                        <Text style={styles.profileDescription}>{Description}</Text>
+                        <Image source={{ uri: Img3 }} style={styles.image} />
+                    </View>
+                    <Image source={{ uri: Img4 }} style={styles.profileBottomImg} />
+                </>
+            )}
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    profile: {
+        flex: 1,
+        padding: 15,
+        alignItems: 'center'
+    },
+    profileLogo: {
+        width: 100,
+        height: 100,
+        borderRadius: 50
+    },
+    profileOrgname: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginVertical: 10
+    },
+    profileMaincontent: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+        marginBottom: 10
+    },
+    profileContact: {
+        fontSize: 16
+    },
+    profileDescription: {
+        fontSize: 14
+    },
+    image: {
+        width: 50,
+        height: 50
+    },
+    profileBottomImg: {
+        width: '100%',
+        height: 150
+    }
+});
 
 export default ProfileView;
